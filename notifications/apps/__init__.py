5df894ff142ac545bd74e22c09c0641dde58eae9
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-08-09 15:42:54 +0100

    Fix AppConfig

--- notifications/apps/__init__.py
+++ notifications/apps/__init__.py
@@ -1,4 +1,19 @@
-from .usernotifications_conf import UserNotificationsConfApp
-from .notifications_types import NotificationsTypesApp
-from .notifications import NotificationsApp
-from .user_notifications import UserNotificationsApp
+from django.apps import AppConfig
+
+
+class NotificationsConfig(AppConfig):
+    name = "notifications"
+    verbose_name = "Notifications"
+
+    def ready(self):
+        # from .. import signals
+
+        from .usernotifications_conf import UserNotificationsConfApp
+        from .notifications_types import NotificationsTypesApp
+        from .notifications import NotificationsApp
+        from .user_notifications import UserNotificationsApp
+
+        global UserNotificationsConfApp
+        global NotificationsTypesApp
+        global NotificationsApp
+        global UserNotificationsApp
