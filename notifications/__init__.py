36e8baf5ad3595fdec202be00353895b4b4312b2
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-08-09 16:17:56 +0100

    Add default_app_config

---
+++ notifications/__init__.py
@@ -0,0 +1,4 @@
+# FIXME remove this. Django recommends:
+#  it’s best to avoid using default_app_config and instead specify the
+# app config class in INSTALLED_APPS as described next.
+default_app_config = 'notifications.apps.NotificationsConfig'
